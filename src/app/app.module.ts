9a1b513a658e77000bf1b3ad794c95d792ddf927
Victor Lima <victor.lima@atlasinovacoes.com.br>
2022-03-27 21:05:07 -0300

    feat: added @ngrx/platform packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 import { LoginModule } from './features/login/login.module';
 
 registerLocaleData(pt);
@@ -27,6 +33,11 @@ registerLocaleData(pt);
         BrowserAnimationsModule,
 
         LoginModule,
+
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        EffectsModule.forRoot([]),
+        StoreRouterConnectingModule.forRoot(),
     ],
     providers: [{ provide: NZ_I18N, useValue: pt_BR }],
     bootstrap: [AppComponent]
