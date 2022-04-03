c03f607d8b095a0d1f82412e940e2a9e4a9e930b
Victor Lima <victor.lima@atlasinovacoes.com.br>
2022-04-03 11:29:39 -0300

    fix: restart server application and rename folders

--- backend/src/graphql/schema.ts
+++
@@ -1,8 +0,0 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { resolvers } from './resolvers';
-import { typeDefs } from './type-defs';
-
-export const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-});
--- backend/src/server.ts
+++
@@ -1,6 +0,0 @@
-import { createServer } from '@graphql-yoga/node';
-import { schema } from './graphql/schema';
-
-const server = createServer({ schema });
-
-server.start();
